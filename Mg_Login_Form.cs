a93682e779a5ca2cd98a925d21fafd04c8e35f9a
FangFoe <fang-pa@hotmail.com>
2021-03-12 12:32:56 +0700

    Update Mg_Login_Form.cs

--- Mg_Login_Form.cs
+++ Mg_Login_Form.cs
@@ -17,14 +17,13 @@ namespace RestaurantManagement
         public Mg_Login_Form()
         {
             InitializeComponent();
-            T_User.MaxLength = 6;
             T_Pass.PasswordChar = '*';
             T_Pass.MaxLength = 6;
         }
 
         private void BT_Login_Click(object sender, EventArgs e)
         {
-            SqlCommand cmd = new SqlCommand("SELECT Em_User,Em_Password FROM Employee WHERE Em_User='" + T_User.Text.Trim() + "'AND Em_Password = '" + T_Pass.Text.Trim() + "'", con);
+            SqlCommand cmd = new SqlCommand("SELECT Em_Password FROM Employee WHERE Em_Password = '" + T_Pass.Text.Trim() + "'", con);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable lgin = new DataTable();
             da.Fill(lgin);
@@ -33,21 +32,13 @@ namespace RestaurantManagement
                 Mg_Path_To_Em_And_F mg_Path_To_Em_And_F = new Mg_Path_To_Em_And_F(this);
                 mg_Path_To_Em_And_F.Show();
                 Hide();
-                T_User.Text = T_Pass.Text = null;
+                T_Pass.Text = null;
 
             }
             else
             {
                 MessageBox.Show("Check Your User And Password", "WARNING!!", MessageBoxButtons.OK);
-                T_User.Text = T_Pass.Text = null;
-            }
-        }
-
-        private void T_User_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
-            {
-                e.Handled = true;
+                T_Pass.Text = null;
             }
         }
 
@@ -57,6 +48,11 @@ namespace RestaurantManagement
             {
                 e.Handled = true;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
