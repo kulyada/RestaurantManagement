32b278142a3570338887e4197ff389766b78d61b
FangFoe <fang-pa@hotmail.com>
2021-03-12 16:50:48 +0700

    Update Mg_F_Form.Designer.cs

--- Mg_F_Form.Designer.cs
+++ Mg_F_Form.Designer.cs
@@ -38,21 +38,20 @@ namespace RestaurantManagement
             this.L_TFood = new System.Windows.Forms.Label();
             this.BT_UpLoadPic = new System.Windows.Forms.Button();
             this.Sh_F_Data = new System.Windows.Forms.DataGridView();
-            this.CO_SL_T_F = new System.Windows.Forms.ComboBox();
             this.P_Food = new System.Windows.Forms.PictureBox();
             this.Bt_Pic = new System.Windows.Forms.Button();
             this.L_Show = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.BT_Log_Out = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.Bt_to_T = new System.Windows.Forms.Button();
+            this.Bt_to_F = new System.Windows.Forms.Button();
             this.Bt_to_Employ = new System.Windows.Forms.Button();
             this.Bt_to_Chef = new System.Windows.Forms.Button();
             this.Bt_To_Cashier = new System.Windows.Forms.Button();
             this.BT_Clear = new System.Windows.Forms.Button();
-            this.BT_Update = new System.Windows.Forms.Button();
             this.BT_Save = new System.Windows.Forms.Button();
-            this.BT_Log_Out = new System.Windows.Forms.Button();
+            this.Bt_Refresh = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Sh_F_Data)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.P_Food)).BeginInit();
             this.panel2.SuspendLayout();
@@ -63,7 +62,7 @@ namespace RestaurantManagement
             // 
             this.L_FName.AutoSize = true;
             this.L_FName.Font = new System.Drawing.Font("Andalus", 22.2F);
-            this.L_FName.Location = new System.Drawing.Point(712, 441);
+            this.L_FName.Location = new System.Drawing.Point(713, 391);
             this.L_FName.Name = "L_FName";
             this.L_FName.Size = new System.Drawing.Size(116, 58);
             this.L_FName.TabIndex = 0;
@@ -73,7 +72,7 @@ namespace RestaurantManagement
             // 
             this.L_FPrice.AutoSize = true;
             this.L_FPrice.Font = new System.Drawing.Font("Andalus", 22.2F);
-            this.L_FPrice.Location = new System.Drawing.Point(712, 487);
+            this.L_FPrice.Location = new System.Drawing.Point(713, 437);
             this.L_FPrice.Name = "L_FPrice";
             this.L_FPrice.Size = new System.Drawing.Size(102, 58);
             this.L_FPrice.TabIndex = 1;
@@ -84,7 +83,7 @@ namespace RestaurantManagement
             this.Co_TFood.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.Co_TFood.Font = new System.Drawing.Font("TH SarabunPSK", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Co_TFood.FormattingEnabled = true;
-            this.Co_TFood.Location = new System.Drawing.Point(869, 557);
+            this.Co_TFood.Location = new System.Drawing.Point(880, 507);
             this.Co_TFood.Name = "Co_TFood";
             this.Co_TFood.Size = new System.Drawing.Size(159, 48);
             this.Co_TFood.TabIndex = 2;
@@ -92,7 +91,7 @@ namespace RestaurantManagement
             // T_FPrice
             // 
             this.T_FPrice.Font = new System.Drawing.Font("TH SarabunPSK", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.T_FPrice.Location = new System.Drawing.Point(869, 502);
+            this.T_FPrice.Location = new System.Drawing.Point(880, 454);
             this.T_FPrice.Name = "T_FPrice";
             this.T_FPrice.Size = new System.Drawing.Size(159, 47);
             this.T_FPrice.TabIndex = 3;
@@ -101,16 +100,16 @@ namespace RestaurantManagement
             // T_FName
             // 
             this.T_FName.Font = new System.Drawing.Font("TH SarabunPSK", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.T_FName.Location = new System.Drawing.Point(869, 444);
+            this.T_FName.Location = new System.Drawing.Point(880, 394);
             this.T_FName.Name = "T_FName";
-            this.T_FName.Size = new System.Drawing.Size(275, 47);
+            this.T_FName.Size = new System.Drawing.Size(265, 47);
             this.T_FName.TabIndex = 4;
             // 
             // L_TFood
             // 
             this.L_TFood.AutoSize = true;
             this.L_TFood.Font = new System.Drawing.Font("Andalus", 22.2F);
-            this.L_TFood.Location = new System.Drawing.Point(680, 547);
+            this.L_TFood.Location = new System.Drawing.Point(681, 497);
             this.L_TFood.Name = "L_TFood";
             this.L_TFood.Size = new System.Drawing.Size(183, 58);
             this.L_TFood.TabIndex = 5;
@@ -119,7 +118,7 @@ namespace RestaurantManagement
             // BT_UpLoadPic
             // 
             this.BT_UpLoadPic.Font = new System.Drawing.Font("Andalus", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BT_UpLoadPic.Location = new System.Drawing.Point(1153, 444);
+            this.BT_UpLoadPic.Location = new System.Drawing.Point(1154, 394);
             this.BT_UpLoadPic.Name = "BT_UpLoadPic";
             this.BT_UpLoadPic.Size = new System.Drawing.Size(115, 50);
             this.BT_UpLoadPic.TabIndex = 6;
@@ -130,28 +129,17 @@ namespace RestaurantManagement
             // Sh_F_Data
             // 
             this.Sh_F_Data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Sh_F_Data.Location = new System.Drawing.Point(377, 127);
+            this.Sh_F_Data.Location = new System.Drawing.Point(377, 167);
             this.Sh_F_Data.Name = "Sh_F_Data";
             this.Sh_F_Data.RowHeadersWidth = 51;
             this.Sh_F_Data.RowTemplate.Height = 24;
-            this.Sh_F_Data.Size = new System.Drawing.Size(297, 581);
+            this.Sh_F_Data.Size = new System.Drawing.Size(297, 541);
             this.Sh_F_Data.TabIndex = 7;
             this.Sh_F_Data.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Sh_F_Data_CellContentClick);
             // 
-            // CO_SL_T_F
-            // 
-            this.CO_SL_T_F.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.CO_SL_T_F.Font = new System.Drawing.Font("TH SarabunPSK", 16.2F, System.Drawing.FontStyle.Bold);
-            this.CO_SL_T_F.FormattingEnabled = true;
-            this.CO_SL_T_F.Location = new System.Drawing.Point(1099, 127);
-            this.CO_SL_T_F.Name = "CO_SL_T_F";
-            this.CO_SL_T_F.Size = new System.Drawing.Size(173, 44);
-            this.CO_SL_T_F.TabIndex = 8;
-            this.CO_SL_T_F.SelectedIndexChanged += new System.EventHandler(this.CO_SL_T_F_SelectedIndexChanged);
-            // 
             // P_Food
             // 
-            this.P_Food.Location = new System.Drawing.Point(689, 177);
+            this.P_Food.Location = new System.Drawing.Point(690, 127);
             this.P_Food.Name = "P_Food";
             this.P_Food.Size = new System.Drawing.Size(583, 261);
             this.P_Food.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -193,11 +181,27 @@ namespace RestaurantManagement
             this.panel2.Size = new System.Drawing.Size(1296, 124);
             this.panel2.TabIndex = 64;
             // 
+            // BT_Log_Out
+            // 
+            this.BT_Log_Out.FlatAppearance.BorderSize = 0;
+            this.BT_Log_Out.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BT_Log_Out.Font = new System.Drawing.Font("Andalus", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_Log_Out.Image = ((System.Drawing.Image)(resources.GetObject("BT_Log_Out.Image")));
+            this.BT_Log_Out.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BT_Log_Out.Location = new System.Drawing.Point(1082, 22);
+            this.BT_Log_Out.Name = "BT_Log_Out";
+            this.BT_Log_Out.Size = new System.Drawing.Size(190, 80);
+            this.BT_Log_Out.TabIndex = 67;
+            this.BT_Log_Out.Text = "Log Out";
+            this.BT_Log_Out.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BT_Log_Out.UseVisualStyleBackColor = true;
+            this.BT_Log_Out.Click += new System.EventHandler(this.BT_Log_Out_Click);
+            // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(156)))), ((int)(((byte)(206)))));
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.Bt_to_T);
+            this.panel1.Controls.Add(this.Bt_to_F);
             this.panel1.Controls.Add(this.Bt_to_Employ);
             this.panel1.Controls.Add(this.Bt_to_Chef);
             this.panel1.Controls.Add(this.Bt_To_Cashier);
@@ -206,35 +210,35 @@ namespace RestaurantManagement
             this.panel1.Size = new System.Drawing.Size(375, 733);
             this.panel1.TabIndex = 66;
             // 
-            // button2
+            // Bt_to_T
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(206)))), ((int)(((byte)(219)))));
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Andalus", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(3, 450);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(372, 117);
-            this.button2.TabIndex = 67;
-            this.button2.Text = "TABLE";
-            this.button2.UseVisualStyleBackColor = false;
+            this.Bt_to_T.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(206)))), ((int)(((byte)(219)))));
+            this.Bt_to_T.FlatAppearance.BorderSize = 0;
+            this.Bt_to_T.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bt_to_T.Font = new System.Drawing.Font("Andalus", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bt_to_T.Image = ((System.Drawing.Image)(resources.GetObject("Bt_to_T.Image")));
+            this.Bt_to_T.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Bt_to_T.Location = new System.Drawing.Point(3, 450);
+            this.Bt_to_T.Name = "Bt_to_T";
+            this.Bt_to_T.Size = new System.Drawing.Size(372, 117);
+            this.Bt_to_T.TabIndex = 67;
+            this.Bt_to_T.Text = "TABLE";
+            this.Bt_to_T.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // Bt_to_F
             // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(206)))), ((int)(((byte)(219)))));
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Andalus", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(3, 341);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(372, 117);
-            this.button3.TabIndex = 66;
-            this.button3.Text = "FOOD";
-            this.button3.UseVisualStyleBackColor = false;
+            this.Bt_to_F.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(206)))), ((int)(((byte)(219)))));
+            this.Bt_to_F.FlatAppearance.BorderSize = 0;
+            this.Bt_to_F.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bt_to_F.Font = new System.Drawing.Font("Andalus", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bt_to_F.Image = ((System.Drawing.Image)(resources.GetObject("Bt_to_F.Image")));
+            this.Bt_to_F.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Bt_to_F.Location = new System.Drawing.Point(3, 341);
+            this.Bt_to_F.Name = "Bt_to_F";
+            this.Bt_to_F.Size = new System.Drawing.Size(372, 117);
+            this.Bt_to_F.TabIndex = 66;
+            this.Bt_to_F.Text = "FOOD";
+            this.Bt_to_F.UseVisualStyleBackColor = false;
             // 
             // Bt_to_Employ
             // 
@@ -285,25 +289,12 @@ namespace RestaurantManagement
             // 
             this.BT_Clear.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.BT_Clear.Font = new System.Drawing.Font("Andalus", 22.2F);
-            this.BT_Clear.Location = new System.Drawing.Point(1034, 515);
+            this.BT_Clear.Location = new System.Drawing.Point(1045, 468);
             this.BT_Clear.Name = "BT_Clear";
             this.BT_Clear.Size = new System.Drawing.Size(147, 67);
             this.BT_Clear.TabIndex = 66;
             this.BT_Clear.Text = "Clear";
             this.BT_Clear.UseVisualStyleBackColor = false;
-            // 
-            // BT_Update
-            // 
-            this.BT_Update.BackColor = System.Drawing.Color.DarkBlue;
-            this.BT_Update.FlatAppearance.BorderSize = 0;
-            this.BT_Update.Font = new System.Drawing.Font("Andalus", 22.2F);
-            this.BT_Update.ForeColor = System.Drawing.Color.White;
-            this.BT_Update.Location = new System.Drawing.Point(1000, 642);
-            this.BT_Update.Name = "BT_Update";
-            this.BT_Update.Size = new System.Drawing.Size(147, 67);
-            this.BT_Update.TabIndex = 65;
-            this.BT_Update.Text = "Update";
-            this.BT_Update.UseVisualStyleBackColor = false;
             // 
             // BT_Save
             // 
@@ -316,34 +307,29 @@ namespace RestaurantManagement
             this.BT_Save.TabIndex = 64;
             this.BT_Save.Text = "Save";
             this.BT_Save.UseVisualStyleBackColor = false;
+            this.BT_Save.Click += new System.EventHandler(this.BT_Save_Click);
             // 
-            // BT_Log_Out
+            // Bt_Refresh
             // 
-            this.BT_Log_Out.FlatAppearance.BorderSize = 0;
-            this.BT_Log_Out.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BT_Log_Out.Font = new System.Drawing.Font("Andalus", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BT_Log_Out.Image = ((System.Drawing.Image)(resources.GetObject("BT_Log_Out.Image")));
-            this.BT_Log_Out.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.BT_Log_Out.Location = new System.Drawing.Point(1080, 22);
-            this.BT_Log_Out.Name = "BT_Log_Out";
-            this.BT_Log_Out.Size = new System.Drawing.Size(192, 80);
-            this.BT_Log_Out.TabIndex = 67;
-            this.BT_Log_Out.Text = "Log Out";
-            this.BT_Log_Out.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.BT_Log_Out.UseVisualStyleBackColor = true;
+            this.Bt_Refresh.FlatAppearance.BorderSize = 0;
+            this.Bt_Refresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bt_Refresh.Image = ((System.Drawing.Image)(resources.GetObject("Bt_Refresh.Image")));
+            this.Bt_Refresh.Location = new System.Drawing.Point(641, 127);
+            this.Bt_Refresh.Name = "Bt_Refresh";
+            this.Bt_Refresh.Size = new System.Drawing.Size(33, 34);
+            this.Bt_Refresh.TabIndex = 67;
+            this.Bt_Refresh.UseVisualStyleBackColor = true;
             // 
             // Mg_F_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(234)))), ((int)(((byte)(245)))));
             this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.Bt_Refresh);
             this.Controls.Add(this.BT_Clear);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.BT_Update);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.BT_Save);
-            this.Controls.Add(this.CO_SL_T_F);
             this.Controls.Add(this.Sh_F_Data);
             this.Controls.Add(this.BT_UpLoadPic);
             this.Controls.Add(this.L_TFood);
@@ -380,20 +366,19 @@ namespace RestaurantManagement
         private System.Windows.Forms.Label L_TFood;
         private System.Windows.Forms.Button BT_UpLoadPic;
         private System.Windows.Forms.DataGridView Sh_F_Data;
-        private System.Windows.Forms.ComboBox CO_SL_T_F;
         private System.Windows.Forms.PictureBox P_Food;
         private System.Windows.Forms.Button Bt_Pic;
         private System.Windows.Forms.Label L_Show;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Bt_to_T;
+        private System.Windows.Forms.Button Bt_to_F;
         private System.Windows.Forms.Button Bt_to_Employ;
         private System.Windows.Forms.Button Bt_to_Chef;
         private System.Windows.Forms.Button Bt_To_Cashier;
         private System.Windows.Forms.Button BT_Clear;
-        private System.Windows.Forms.Button BT_Update;
         private System.Windows.Forms.Button BT_Save;
         private System.Windows.Forms.Button BT_Log_Out;
+        private System.Windows.Forms.Button Bt_Refresh;
     }
 }
